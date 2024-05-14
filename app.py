d7352e401491bd475dce86583fb05f62c0605ac2
inamprograms <inamul09rehman@gmail.com>
2024-05-14 14:57:46 +0500

    Project setup done

---
+++ app.py
@@ -0,0 +1,15 @@
+# streamlit app.py
+
+import streamlit as st
+def main():
+   
+    st.write("""
+        # AI Verse
+        Your *tourism* and *cultural preservation* App
+    """)
+    st.sidebar.title("Side Bar title")
+
+   
+
+if __name__ == "__main__":
+    main()
